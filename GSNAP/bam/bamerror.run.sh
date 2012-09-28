11b5e075c1c6fa6bc8d28256bd54bc55b8f75e94
alvarani <alva.rani@scilifelab.se>
2012-09-28 14:00:16 +0200

    Added new GSNAP

---
+++ GSNAP/bam/bamerror.run.sh
@@ -0,0 +1,31 @@
+
+###Test for 19 sam file
+#created a errorlist of 19 files
+# taking the the reads appreaed more than twice and taking it in a order 
+cat errorfiles.list | xargs -I% echo "samtools view "%" | awk '{print \$1;}' | sort | uniq -c >"%.reads2nmapped >cmds.sh
+
+
+
+errordir='/proj/b2012046/rani/analysis/gsnap/bamsort/errortest'
+
+ls -1 ${errordir}/*.reads2nmapped >reads2nmapped.files
+
+cat reads2nmapped.files | xargs -I% basename % | xargs -I% echo "awk '\$1 == 2 {print \$0;}'" ${errordir}/% " | wc -l >" %.n.uniq >cmds.sh
+cat reads2nmapped.files | xargs -I% basename % | xargs -I% echo "awk '\$1 != 2 {print \$0;}'" ${errordir}/% " | wc -l >" %.n.nonuniq >cmds2.sh
+
+
+bamfiles='/proj/b2012046/rani/analysis/gsnap/bamsort/errortest/errorbamfiles.list'
+errordir='/proj/b2012046/rani/analysis/gsnap/bamsort/errortest'
+
+#Extract multimapped reads
+ls -1 ${errordir}/*.reads2nmapped >reads2nmapped.files
+cat reads2nmapped.files | xargs -I% basename % | xargs -I% echo "awk '\$1 != 2 {print \$0;}'" ${errordir}/% "| awk '{print \$2;}'>" %.nonuniq >cmds.sh
+sh cmds.sh &
+
+#TBD: Convert all bams to sams
+bamdir='/proj/b2012046/rani/analysis/gsnap/bamindex'
+sam=${bamdir}/2_LPS.part_2ab.sam
+
+#Rm multimapped reads
+ls -1 *d.nonuniq | awk -F'.' -v OFS='.' '{print $1, $2;}' >errorfiles.prefix.list
+cat errorfiles.prefix.list | xargs -I% echo "perl rm.multimappedreads.pl " %.sam %.fastq.bam.reads2nmapped.nonuniq ">"%.uniqmapped.sam >cmds.sh
